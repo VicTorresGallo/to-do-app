12f00ff19044e6c130107ff4855cd9e2908ab12f
VicTorresGallo <vicentg1211@gmail.com>
2024-04-21 17:29:15 +0200

    Formulario de acceso o Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,38 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { LoginComponent } from './components/login/login.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { TasksComponent } from './components/tasks/tasks.component';
-import { AuthInterceptor } from './interceptors/auth.interceptor';
-@NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    NavbarComponent,
-    TasksComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule,
-    HttpClientModule
-  ],
-  providers: [
-    provideClientHydration(),
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptor,
-      multi: true
-    }
-  ],
-  bootstrap: [AppComponent]
-})
+import { NgModule } from '@angular/core'; 
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser'; 
+ 
+import { AppRoutingModule } from './app-routing.module'; 
+import { AppComponent } from './app.component'; 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'; 
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'; 
+ 
+import { LoginComponent } from './components/login/login.component'; 
+import { NavbarComponent } from './components/navbar/navbar.component'; 
+import { TasksComponent } from './components/tasks/tasks.component'; 
+ 
+import { AuthInterceptor } from './interceptors/auth.interceptor'; 
+ 
+@NgModule({ 
+  declarations: [ 
+    AppComponent, 
+    LoginComponent, 
+    NavbarComponent, 
+    TasksComponent 
+  ], 
+  imports: [ 
+    BrowserModule, 
+    AppRoutingModule, 
+    NgbModule, 
+    HttpClientModule 
+  ], 
+  providers: [ 
+    provideClientHydration(), 
+    { 
+      provide: HTTP_INTERCEPTORS, 
+      useClass: AuthInterceptor, 
+      multi: true 
+    } 
+  ], 
+  bootstrap: [AppComponent] 
+}) 
 export class AppModule { }
