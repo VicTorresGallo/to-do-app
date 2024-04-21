43a4c5d4f6cd444596fb10592e117e6a4fcd5d27
VicTorresGallo <vicentg1211@gmail.com>
2024-04-21 14:26:24 +0200

    Estructura basica de proyecto:login+taskbar+navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from "./components/login/login.component";
+import { TasksComponent } from "./components/tasks/tasks.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',
+  component: TasksComponent },
+  { path: 'login', component: LoginComponent },
+  
+  // Redirecciones
+  { path: 'home', redirectTo: '' },
+  { path: '**', redirectTo: '' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
